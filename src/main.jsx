416d6ab356c57d879242ed9d9e58c7a14084de27
Rodrigo <rdg.hora1@gmail.com>
2023-11-06 08:33:32 -0300

    Configurando o styled-components

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { GlobalStyles } from "./styles/global-styles.js";
 import { Home } from "./templates/Home/index.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <GlobalStyles />
     <Home />
   </React.StrictMode>,
 );
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,7 +1,11 @@
+import * as Styled from "./styles.js";
+
 export const Home = () => {
   return (
     <>
-      <h1>Hello World!</h1>
+      <Styled.Wrapper>
+        <h1>Hello World!</h1>
+      </Styled.Wrapper>
     </>
   );
 };
