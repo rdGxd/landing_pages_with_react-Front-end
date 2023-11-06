589b3d943c17c3532fcdb328ca8dfed4d052bdaa
Rodrigo <rdg.hora1@gmail.com>
2023-11-06 11:03:18 -0300

    Storybook Part 1

---
+++ .storybook/main.js
@@ -0,0 +1,22 @@
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)", "../src/**/stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+  ],
+  core: {
+    builder: "@storybook/builder-vite", // 👈 The builder enabled here.
+  },
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
