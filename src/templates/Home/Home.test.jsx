9c534965815157b8a26e80e02aec6c6e706515e4
Rodrigo <rdg.hora1@gmail.com>
2023-11-07 10:47:23 -0300

    testando o componente heading

--- src/templates/Home/Home.test.jsx
+++ src/templates/Home/Home.test.jsx
@@ -1,19 +1,8 @@
-import { screen } from "@testing-library/react";
 import { Home } from ".";
 import { renderTheme } from "../../styles/render-theme";
-import { theme } from "../../styles/theme";
 
-test("renders learn react link", () => {
-  renderTheme(<Home />);
-  const headingContainer = screen.getByRole("heading", { name: "Hello World!" }).parentElement;
-
-  expect(headingContainer).toHaveStyle({
-    background: theme.colors.secondaryBg,
+describe("</Home", () => {
+  it("should render home", () => {
+    renderTheme(<Home />);
   });
-
-  expect(headingContainer).toHaveStyleRule({
-    background: "red",
-  });
-
-  expect(headingContainer).toMatchSnapshot();
 });
