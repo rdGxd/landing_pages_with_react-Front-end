24b2235b0fe64ae1ec25a8bf9c457cb3923e73ad
Rodrigo <rdg.hora1@gmail.com>
2023-11-09 10:56:56 -0300

    Testes: Menu

--- src/components/Menu/Menu.test.jsx
+++ src/components/Menu/Menu.test.jsx
@@ -2,9 +2,47 @@ import { screen } from "@testing-library/react";
 import { Menu } from ".";
 import { renderTheme } from "../../styles/render-theme";
 
+import { fireEvent } from "@storybook/testing-library";
+import linksMocks from "../NavLinks/mock";
+const logoData = {
+  text: "Logo",
+  link: "#target",
+};
+
 describe("<Menu />", () => {
-  it("should render", () => {
-    renderTheme(<Menu>Children</Menu>);
-    expect(screen.getByRole("heading")).toBeInTheDocument();
+  it("should render Logo and Main menu nav", () => {
+    const { container } = renderTheme(<Menu links={linksMocks} logoData={logoData} />);
+    expect(screen.getByRole("heading", { name: "Logo" })).toBeInTheDocument();
+
+    expect(screen.getByRole("navigation", { name: "Main menu" })).toBeInTheDocument();
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it("should render menu mobile and button for open and close the menu", () => {
+    const { container } = renderTheme(<Menu links={linksMocks} logoData={logoData} />);
+
+    const button = screen.getByLabelText("Open/Close menu");
+    const menuContainer = button.nextSibling;
+
+    expect(button).toHaveStyle("display: none");
+
+    // expect(button).toHaveStyleRule(`media: ${theme.media.lteMedium}`, "display: flex");
+
+    // expect(button).toHaveStyleRule(`media: ${theme.media.lteMedium}`, "opacity: 0");
+    expect(screen.getByLabelText("Open menu")).toBeInTheDocument();
+
+    fireEvent.click(button);
+    expect(screen.getByLabelText("Close menu")).toBeInTheDocument();
+
+    fireEvent.click(menuContainer);
+    expect(screen.getByLabelText("Open menu")).toBeInTheDocument();
+  });
+
+  it("should not render links", () => {
+    const { container } = renderTheme(<Menu logoData={logoData} />);
+    expect(screen.queryByRole("navigation", { name: "Main menu" }).firstChild).not.toBeInTheDocument();
+
+    expect(container).toMatchSnapshot();
   });
 });
--- src/components/NavLinks/index.jsx
+++ src/components/NavLinks/index.jsx
@@ -4,12 +4,10 @@ import * as Styled from "./styles";
 
 export const NavLinks = ({ links = [] }) => {
   return (
-    <Styled.Container>
-      <h1>
-        {links.map((link) => (
-          <MenuLink key={link.link} {...link} />
-        ))}
-      </h1>
+    <Styled.Container aria-label="Main menu">
+      {links.map((link) => (
+        <MenuLink key={link.link} {...link} />
+      ))}
     </Styled.Container>
   );
 };
