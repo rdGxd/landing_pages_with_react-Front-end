6ca56fe301101bf2ef151267a9116e115aee37d5
Rodrigo <rdg.hora1@gmail.com>
2023-11-10 12:54:38 -0300

    Componente Go to top

---
+++ src/components/GoTop/stories.jsx
@@ -0,0 +1,201 @@
+import { GoTop } from ".";
+
+export default {
+  title: "GoTop",
+  component: GoTop,
+  args: {
+    children: "GoTop",
+  },
+  argTypes: {
+    children: { type: "string" },
+  },
+};
+
+export const Template = (args) => {
+  return (
+    <div style={{ height: "400vh" }}>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, natus accusantium! Possimus consequatur
+        voluptate tempore animi magni quibusdam illo, iste dolorem in sequi fuga inventore veniam obcaecati quae qui
+        reiciendis!
+      </p>
+      <GoTop {...args} />
+    </div>
+  );
+};
