d40ea2ba646333c96cd30ea4af0af9e47e6d0f9b
Rodrigo <rdg.hora1@gmail.com>
2023-11-10 11:17:52 -0300

    Componente com testes: Grid Image

--- src/components/GridImage/GridImage.test.jsx
+++ src/components/GridImage/GridImage.test.jsx
@@ -1,10 +1,16 @@
-import { screen } from "@testing-library/react";
 import { GridImage } from ".";
 import { renderTheme } from "../../styles/render-theme";
 
+import mock from "./mock";
+
 describe("<GridImage />", () => {
-  it("should render", () => {
-    renderTheme(<GridImage>Children</GridImage>);
-    expect(screen.getByRole("heading")).toBeInTheDocument();
+  it("should render with images and content", () => {
+    const { container } = renderTheme(<GridImage {...mock} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it("should render with background undefined", () => {
+    const { container } = renderTheme(<GridImage {...mock} background={undefined} />);
+    expect(container).toMatchSnapshot();
   });
 });
