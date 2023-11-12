65052efcd893807f7b26ff96319481d654bf37e9
Rodrigo <rdg.hora1@gmail.com>
2023-11-12 08:28:05 -0300

    Carregando dados da API

--- src/api/map-data.js
+++ src/api/map-data.js
@@ -2,8 +2,8 @@ import { mapMenu } from "./map-menu";
 import { mapSections } from "./map-sections";
 
 export const mapData = (pagesData = [{}]) => {
-  return pagesData.map((data) => {
-    const { footer_text: footerHtml = "", slug = "", title = "", sections = [], menu = {} } = data;
+  return pagesData.map(({ attributes }) => {
+    const { footer_text: footerHtml = "", slug = "", title = "", sections = [], menu = {} } = attributes;
 
     return {
       footerHtml,
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,11 +1,34 @@
+import { useEffect, useState } from "react";
+import { mapData } from "../../api/map-data.js";
 import { Base } from "../Base/index.jsx";
 import { mock } from "../Base/mock.jsx";
-import * as Styled from "./styles.js";
 
-export const Home = () => {
-  return (
-    <Styled.Wrapper>
-      <Base {...mock} />
-    </Styled.Wrapper>
-  );
+const Home = () => {
+  const [response, setResponse] = useState([]);
+
+  useEffect(() => {
+    const load = async () => {
+      const data = await fetch(
+        "http://localhost:1337/api/pages/?filters=[slug]=landing-page&populate[sections][populate]=*",
+      );
+      const json = await data.json();
+      const pageData = mapData(json.data);
+      setResponse(pageData[0]);
+    };
+
+    load();
+  }, []);
+
+  console.log(response);
+  if (response === undefined) {
+    return <h1>Página não encontrada</h1>;
+  }
+
+  if (response && !response.slug) {
+    return <h1>Página não encontrada</h1>;
+  }
+
+  return <Base {...mock} />;
 };
+
+export default Home;
